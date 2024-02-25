4f48f3456834e8e658b3920e484e9a84cc61c423
phillomella <121259223+phillomella@users.noreply.github.com>
2024-02-25 16:09:01 +0300

    Navigation buttons

--- app/src/main/java/ui/components/AppDrawer.kt
+++ app/src/main/java/ui/components/AppDrawer.kt
@@ -10,12 +10,12 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.*
 import androidx.compose.material.icons.filled.Menu
-//import androidx.compose.material.icons.filled.Home
-//import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Delete
 
-//import androidx.compose.foundation.clickable
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.ComposableOpenTarget
+import androidx.compose.runtime.ComposableOpenTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,12 +23,11 @@ import androidx.compose.ui.unit.dp
 
 
 import androidx.compose.foundation.layout.*
-
-
-
-
-
-
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.topic2.android.notes.routing.NotesRouter
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.theme.NotesThemeSettings
 
 
 @Composable
@@ -46,5 +45,71 @@ private fun AppDrawerHeader(){
                 .align(alignment = Alignment.CenterVertically))
     }
 
+}
+@Preview
+@Composable
+fun AppDrawerHeaderPreview(){
+    NotesTheme{
+        AppDrawerHeader()
+    }
+}
+
+@Composable
+private fun ScreenNavigationButton(
+    icon: ImageVector,
+    label: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    val colors = MaterialTheme.colors
+
+    val imageAlpha = if (isSelected){
+        1f
+    } else{
+        0.6f
+    }
+
+    val textColor = if (isSelected){
+        colors.primary
+    } else{
+        colors.onSurface.copy(alpha = 0.6f)
+    }
+
+    val backgroundColor = if (isSelected){
+        colors.primary.copy(alpha = 0.12f)
+    } else{
+        colors.surface
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+        color = backgroundColor,
+        shape = MaterialTheme.shapes.small
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(onClick = onClick)
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Image(
+                imageVector = icon ,
+                contentDescription = "Screen Navigation Button",
+                colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(textColor),
+                alpha = imageAlpha
+            )
+            Spacer(Modifier.width(16.dp))
+            Text(
+                text = label,
+                style = MaterialTheme.typography.body2,
+                color = textColor,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
 }
 
