70f9d673091dd7e9864632ec95b0f31a27b36e12
phillomella <121259223+phillomella@users.noreply.github.com>
2024-02-26 17:41:15 +0300

    updNote

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -47,15 +47,9 @@ fun Note() {
 
     ) {
 
-        NoteColor(
-            modifier = Modifier.align(Alignment.CenterVertically),
-            color = rwGreen,
-            size = 40.dp,
-            padding = 4.dp,
-            border = 1.dp
-
-
-        )
+        NoteColor(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .padding(start = 16.dp, end = 16.dp), rwGreen, 40.dp, 1.dp)
 
         Column(modifier = Modifier
             .weight(1f)
--- app/src/main/java/ui/components/NoteColor.kt
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -4,46 +4,45 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.RowScopeInstance.align
+//import androidx.compose.foundation.layout.RowScopeInstance.align
+//import androidx.compose.foundation.layout.RowScopeInstance.align
+//import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.Dp
+//import androidx.compose.ui.unit.dp
+//import com.topic2.android.notes.theme.rwGreen
+val rwGreen = Color(0xFF00FF00)
 
 @Composable
-fun NoteColor(
-    modifier: Modifier = Modifier,
-    color: Color,
-    size: Dp,
-    padding: Dp = 0.dp,
-    border: Dp
-){
+fun NoteColor(modifier: Modifier = Modifier, color: Color, size: Dp, border: Dp) {
     Box(
         modifier = modifier
-            .padding(padding)
             .size(size)
             .clip(CircleShape)
             .background(color)
             .border(
                 BorderStroke(
-                    2.dp,
+                    border,
                     SolidColor(Color.Black)
-
                 ),
                 CircleShape
             )
-
     )
 }
 
 @Preview
 @Composable
 fun NoteColorPreview(){
-    NoteColor(color = Color.Red, size = 40.dp, border = 2.dp)
+
 }
