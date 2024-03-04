f03f1538f1629aaf40d34e7e7928e7dbdf5fb187
phillomella <121259223+phillomella@users.noreply.github.com>
2024-03-04 22:28:22 +0300

    SaveNoteScreen/MainActivity

--- app/src/main/java/screens/SaveNoteScreen.kt
+++ app/src/main/java/screens/SaveNoteScreen.kt
@@ -1,14 +1,17 @@
 package screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 //import com.example.notes.domain.model.ColorModel
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,12 +21,45 @@ import ui.components.NoteColor
 import java.lang.reflect.Modifier
 
 @Composable
+private fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+){
+    Column (
+        modifier = androidx.compose.ui.Modifier.fillMaxWidth()
+    ){
+        Text(
+            text = "Color picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = androidx.compose.ui.Modifier.padding(8.dp)
+
+        )
+        LazyColumn(
+            modifier = androidx.compose.ui.Modifier.fillMaxWidth()
+        ){
+            items(
+                colors.size
+            ){
+                    itemIdex ->
+                val color = colors[itemIdex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
 fun ColorItem(
     color: ColorModel,
     onColorSelect: (ColorModel) -> Unit
 ){
     Row(
-        modifier = Modifier
+        modifier = androidx.compose.ui.Modifier
             .fillMaxWidth()
             .clickable(
                 onClick = {
@@ -32,7 +68,7 @@ fun ColorItem(
             )
     ){
         NoteColor(
-            modifier = Modifier
+            modifier = androidx.compose.ui.Modifier
                 .padding(10.dp),
             color = Color.fromHex(color.hex),
             size = 90.dp,
@@ -41,15 +77,28 @@ fun ColorItem(
         Text(
             text = color.name,
             fontSize = 22.sp,
-            modifier = Modifier
+            modifier = androidx.compose.ui.Modifier
                 .padding(horizontal = 16.dp)
                 .align(Alignment.CenterVertically)
         )
 
     }
 }
+
 @Preview
 @Composable
 fun ColorItemPreview(){
     ColorItem(ColorModel.DEFAULT){}
+}
+
+@Preview
+@Composable
+fun ColorPickerPreview(){
+    ColorPicker(
+        colors = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        )
+    ){}
 }
