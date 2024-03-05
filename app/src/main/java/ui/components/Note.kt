9f4f64989978a491330aff94054e851b3b97c649
phillomella <121259223+phillomella@users.noreply.github.com>
2024-03-05 10:19:37 +0300

    upd Note

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -1,16 +1,17 @@
 package ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 //import androidx.compose.foundation.layout.ColumnScopeInstance.weight
 import androidx.compose.foundation.layout.Row
 //import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.runtime.Composable
@@ -19,7 +20,7 @@ import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.topic2.android.notes.R
+//import com.topic2.android.notes.R
 //import androidx.compose.foundation.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
@@ -27,63 +28,89 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 //import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.domain.model.NoteModel
 //import androidx.compose.ui.res.colorResource
 //import androidx.compose.ui.unit.min
 import com.topic2.android.notes.theme.rwGreen
+import com.topic2.android.notes.util.fromHex
 
 //import androidx.compose.ui.modifier.modifierLocalOf
 
 
-@Composable
-fun Note(
+@Composable fun Note(
     note: NoteModel,
     onNoteClick: (NoteModel) -> Unit = {},
     onNoteCheckedChange: (NoteModel) -> Unit = {}
-
 ) {
     val backgroundShape: Shape = RoundedCornerShape(4.dp)
-    Row(modifier = Modifier
-        .padding(8.dp)
-        .shadow(1.dp, backgroundShape)
-        .fillMaxWidth()
-        .heightIn(min = 64.dp)
-        .background(Color.White, backgroundShape)
-
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+            .clickable(onClick = { onNoteClick(note) })
     ) {
-
-        NoteColor(modifier = Modifier
-            .align(Alignment.CenterVertically)
-            .padding(start = 16.dp, end = 16.dp), rwGreen, 40.dp, 1.dp)
-
+        NoteColor(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp, end = 16.dp),
+            color = Color.fromHex(note.color.hex),
+            size = 40.dp,
+            border = 1.dp
+        )
         Column(modifier = Modifier
             .weight(1f)
             .align(Alignment.CenterVertically)
         ) {
-
-            Text(text = "Заголовок", maxLines = 1)
-            Text(text = "Содержимое", maxLines = 1)
+            Text(text = note.title,
+                color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+                )
+            )
+            Text(
+                text = note.content,
+                color = Color.Black.copy(alpha = 0.75f),
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
+                )
+            )
         }
 
-        Checkbox(
-            checked = false,
-            onCheckedChange = {},
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        )
+        if (note.isCheckedOff !=null)
+            Checkbox(
+                checked = note.isCheckedOff,
+                onCheckedChange = {isChecked ->
+                    val newNote = note.copy(isCheckedOff = isChecked)
+                    onNoteCheckedChange(newNote)
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+            )
     }
 }
 
-
 @Preview
 @Composable
-private fun  NotePreview()
-{
-    Note(note = NoteModel(
+private fun NotePreview()
+{ Note(
+    note = NoteModel(
         1,
         "Заметка 1",
         "Содержимое 1",
-        null))
+        null)
+)
 }
 
