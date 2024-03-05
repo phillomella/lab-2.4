5ead96dac7cfeac96c3b9c87faab07f7d330b3a4
phillomella <121259223+phillomella@users.noreply.github.com>
2024-03-06 00:02:02 +0300

    upd Scaffold

--- app/src/main/java/ui/components/screens/NotesScreen.kt
+++ app/src/main/java/ui/components/screens/NotesScreen.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
@@ -46,15 +48,32 @@ fun NotesScreen(
 
 
 
-    Scaffold(topBar = {
-        TopAppBar(title = "Notes", icon = Icons.Filled.List, onIconClick = {
-            coroutineScope.launch{
-                scaffoldState.drawerState.open()
-            }
-        }
-        )
+    Scaffold (
+        topBar = {
+            androidx.compose.material.TopAppBar(
+                title = {
+                    Text(
+                        text = "Notes",
+                        color = MaterialTheme.colors.onPrimary
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                scaffoldState.drawerState.open()
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.List,
+                            contentDescription = "Drawer Button"
+                        )
+                    }
+                }
+            )
+        },
 
-    },
         scaffoldState = scaffoldState,
         drawerContent = {
             AppDrawer(currentScreen = Screen.Notes,
